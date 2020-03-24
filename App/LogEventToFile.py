d56bb3c68a990b27f8195767a54948ca530f5ad0
Ben Cross <bencross86@gmail.com>
2020-03-24 21:46:54 +0000

    Added more details to log file

--- App/LogEventToFile.py
+++ App/LogEventToFile.py
@@ -8,7 +8,12 @@ class LogEventToFile:
     def handle(self, event: AbstractGameAction):
         # TODO: Store the log file name in an overall config/.env file
         with open('event-log.txt', 'a') as eventLog:
-            # TODO: log more details
+            # Minute:Seconds - Player Name - Action
+            line = "%d:%02d - %s - %s" % (
+                event.timeInSeconds / 60,
+                event.timeInSeconds % 60,
+                event.player.name(),
+                type(event).__name__
+            )
 
-            # write the name of the events type to the log file
-            eventLog.write(type(event).__name__ + "\n")
+            eventLog.write(line + "\n")
